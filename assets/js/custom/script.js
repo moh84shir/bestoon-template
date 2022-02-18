de03cfb9e50f081f5d9f5d2d875e6d65205b0c16
moh84shir <moh84shir@gmail.com>
2022-02-18 12:13:27 +0330

    initial commit

---
+++ assets/js/custom/script.js
@@ -0,0 +1,4 @@
+  document.addEventListener('DOMContentLoaded', function() {
+    M.Sidenav.init(document.querySelectorAll('.sidenav'), {});
+    M.Collapsible.init(document.querySelectorAll('.collapsible'), {});
+  });
